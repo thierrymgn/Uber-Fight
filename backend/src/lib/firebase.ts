d3f82c17a72a8a6392e86c53c988f1d170537b6d
Jules Roche <julesro42@gmail.com>
2026-01-12 11:22:04 +0100

    init: add firebase config

---
+++ backend/src/lib/firebase.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAZ3qxW_rdZ8fCSxMai5vsNSpI5wdRzDz8",
+    authDomain: "uber-fight.firebaseapp.com",
+    databaseURL: "https://uber-fight-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "uber-fight",
+    storageBucket: "uber-fight.firebasestorage.app",
+    messagingSenderId: "1089591370423",
+    appId: "1:1089591370423:web:c73cd7d52f67318be0a332",
+    measurementId: "G-5V1Z63ZBZG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
